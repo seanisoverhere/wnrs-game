1c541fde1220ec46edcb121dde81dfebbedb9284
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2024-04-08 22:01:22 +0800

    add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,23 @@ import { useState } from "react";
 // Components
 import Header from "@/components/Header";
 import Auth from "@/components/Auth";
+import Card from "@/components/Card";
 
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <div className="min-h-screen bg-amber-50 p-4">
+    <div className="min-h-screen bg-amber-50 px-4 pb-4">
       <Header isAuthenticated={isAuthenticated} />
       <main className="overflow-hidden">
         {!isAuthenticated ? (
           <Auth setIsAuthenticated={setIsAuthenticated} />
         ) : (
-          <></>
+          <div className="my-16 flex flex-col gap-12 justify-center items-center">
+            <Card cardTitle="Perception" cardLevel="1" isTitleCard />
+            <Card cardTitle="Connection" cardLevel="2" isTitleCard />
+            <Card cardTitle="Reflection" cardLevel="3" isTitleCard />
+          </div>
         )}
       </main>
     </div>
---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+type CardProps = {
+  cardTitle: string;
+  cardLevel: string;
+  isTitleCard: boolean;
+};
+
+const Card = ({ cardTitle, cardLevel, isTitleCard }: CardProps) => {
+  return (
+    <div
+      className={`${
+        isTitleCard ? "bg-red-800 text-white" : "text-red-900"
+      } font-semibold rounded-3xl py-14 text-center w-3/4`}
+    >
+      <span className="text-3xl block my-1">LEVEL {cardLevel}</span>
+      <span className="text-xl uppercase">( {cardTitle} )</span>
+    </div>
+  );
+};
+
+export default Card;
